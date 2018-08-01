4113d7ac7f610232b021cabc483edab60821ddde
trungnx <trung@kyber.network>
2018-08-01 18:08:04 +0700

    taken care of null receive amount

--- src/js/containers/Layout/Layout.js
+++ src/js/containers/Layout/Layout.js
@@ -81,7 +81,7 @@ export default class Layout extends React.Component {
 
     var receiveAddr = common.getParameterByName("receiveAddr")
     var receiveToken = common.getParameterByName("receiveToken")
-    var receiveAmount = common.getParameterByName("receiveAmount").toString();
+    var receiveAmount = common.getParameterByName("receiveAmount");
     // console.log("receiveAmount")
     // console.log(receiveAmount)
     var callback = common.getParameterByName("callback")
@@ -110,6 +110,8 @@ export default class Layout extends React.Component {
     
 
     if (receiveAmount && receiveAmount !== ""){
+      receiveAmount = receiveAmount.toString();
+
       if (isNaN(receiveAmount)) {
         errors["receiveAmount"] = this.props.translate('error.receive_amount_is_invalid_number') 
           || "Receive amount is invalid number"
