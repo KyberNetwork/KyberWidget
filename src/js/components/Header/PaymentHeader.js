5781103554aa3c8f776bf0e687e8b695cd50475d
Andrew Nguyen <cuong.hv.dtvt@gmail.com>
2018-11-26 15:30:12 +0700

    increase version

--- src/js/components/Header/PaymentHeader.js
+++ src/js/components/Header/PaymentHeader.js
@@ -16,7 +16,7 @@ const PaymentHeader = (props) => {
     <div>
       <div className={addPrefixClass("kyber-payment-logo-container")}>
         <div className={addPrefixClass("kyber-payment-logo")}></div>
-        <div className={addPrefixClass("k-version")}>v0.3</div>
+        <div className={addPrefixClass("k-version")}>v0.4</div>
       </div>
       <div className={addPrefixClass("step-breadcrumb k-container")}>
         {props.type === 'swap' && (
