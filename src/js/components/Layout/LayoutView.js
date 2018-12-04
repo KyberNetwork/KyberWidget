eac110f9b27923a46e6f40b0591768e62eadbf59
trungnx <trung@kyber.network>
2018-12-04 15:04:26 +0700

    delete unnecessary codes

--- src/js/components/Layout/LayoutView.js
+++ src/js/components/Layout/LayoutView.js
@@ -2,25 +2,25 @@ import React from "react"
 import { Switch, Route, Redirect } from 'react-router'
 import { ConnectedRouter } from 'react-router-redux'
 import { Processing, InfoModal } from "../../containers/CommonElements/"
-import constansts from "../../services/constants"
+import constant from "../../services/constants"
 import * as common from "../../utils/common"
 
 const LayoutView = (props) => {
-  var defaultPathExchange = constansts.BASE_HOST + constansts.PAYMENT_PATH
+  var defaultPathExchange = constant.BASE_HOST + constant.PAYMENT_PATH
 
   var params = common.queryParamsString(window.location.href)
   if (props.currentLanguage !== "en"){
     defaultPathExchange += "?lang=" + props.currentLanguage
     Object.keys(params).map(key => {
-      defaultPathExchange += `&${key}=${params[key]}` 
+      defaultPathExchange += `&${key}=${params[key]}`
     })
   }else{
-    var index = 0 
+    var index = 0
     Object.keys(params).map(key => {
       if (index === 0){
-        defaultPathExchange += `?${key}=${params[key]}` 
+        defaultPathExchange += `?${key}=${params[key]}`
       }else{
-        defaultPathExchange += `&${key}=${params[key]}` 
+        defaultPathExchange += `&${key}=${params[key]}`
       }
       index ++
     })
@@ -29,13 +29,13 @@ const LayoutView = (props) => {
   return (
     <ConnectedRouter history={props.history}>
       <div>
-        <Route component={props.Header} />
+        <Route/>
         <section id="content">
           {props.paymentHeader}
 
           <Switch>
-            <Route exact path={constansts.BASE_HOST + constansts.PAYMENT_PATH} component={props.Exchange} />
-            <Route exact path={constansts.BASE_HOST + constansts.PAYMENT_PATH +"*"} component={props.Exchange} />
+            <Route exact path={constant.BASE_HOST + constant.PAYMENT_PATH} component={props.Exchange} />
+            <Route exact path={constant.BASE_HOST + constant.PAYMENT_PATH +"*"} component={props.Exchange} />
             <Redirect to={defaultPathExchange} />
           </Switch>
 
--- src/js/containers/Header/index.js
+++
@@ -1,3 +0,0 @@
-export { default as Rate } from './Rate.js';
-export { default as Address } from './Address.js';
-export { default as Notify } from './Notify.js';
