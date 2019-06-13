3cbf7778c2b0591406f941b3bc354f6f353293e0
trungnx <trung@kyber.network>
2019-06-13 17:41:01 +0700

    Update logo for popup mode

--- dist/native/widget.js
+++ dist/native/widget.js
@@ -1,6 +1,6 @@
 (function (global) {
   var WIDGET_VERSION = "0.7.0";
-  var incrementDeploy = 38;
+  var incrementDeploy = 39;
 
   function getUrlParam(name) {
     return new URLSearchParams(location.search).get(name);
